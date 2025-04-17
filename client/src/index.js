ed54986b5082b47415ef2c6c91d65ee448323325
sukhwinderverma <145069602+sukhwinderverma@users.noreply.github.com>
2025-04-17 16:32:34 -0400

    Update index.js

--- client/src/index.js
+++ client/src/index.js
@@ -3,27 +3,50 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
-import { ApolloClient, InMemoryCache, ApolloProvider, HttpLink, ApolloLink } from '@apollo/client'; 
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  HttpLink,
+  ApolloLink,
+} from '@apollo/client';
+import { RetryLink } from '@apollo/client/link/retry';
 
-const client = new ApolloClient({
-
-  link: ApolloLink.from([
-    new HttpLink({
-      uri: 'https://capstone-server2-2qh1.onrender.com/graphql',  
-      credentials: 'include',  
-    }),
-  ]),
-  cache: new InMemoryCache(),
-  headers: {
- 
-    Authorization: `Bearer ${localStorage.getItem('authToken')}`, 
+const retryLink = new RetryLink({
+  delay: {
+    initial: 1000,
+    max: 120000,
+    jitter: true,
+  },
+  attempts: {
+    max: 5,
+    retryIf: (error) => !!error,
   },
 });
 
+const authLink = new ApolloLink((operation, forward) => {
+  const token = localStorage.getItem('authToken');
+  operation.setContext({
+    headers: {
+      Authorization: token ? `Bearer ${token}` : '',
+    },
+  });
+  return forward(operation);
+});
+
+const httpLink = new HttpLink({
+  uri: 'https://capstone-server2-2qh1.onrender.com/graphql',
+  credentials: 'include',
+});
+
+const client = new ApolloClient({
+  link: ApolloLink.from([retryLink, authLink, httpLink]),
+  cache: new InMemoryCache(),
+});
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <ApolloProvider client={client}> 
+  <ApolloProvider client={client}>
     <BrowserRouter>
       <App />
     </BrowserRouter>
