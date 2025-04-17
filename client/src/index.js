81a2398ade0aeddff120464858f1c2c3eee31558
sukhwinderverma <145069602+sukhwinderverma@users.noreply.github.com>
2025-04-17 14:53:00 -0400

    Update index.js

--- client/src/index.js
+++ client/src/index.js
@@ -8,7 +8,7 @@ import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client'; //
 // Set up Apollo Client
 const client = new ApolloClient({
   // uri: 'http://localhost:4005/graphql',
-uri: 'https://capstone-server1-5a59.onrender.com/graphql',
+uri: 'https://capstone-server2-2qh1.onrender.com/graphql',
 
   cache: new InMemoryCache(),
 });
