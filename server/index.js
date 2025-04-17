8de8073f1d939314af88eeb1dace9e96988df9ff
sukhwinderverma <145069602+sukhwinderverma@users.noreply.github.com>
2025-04-17 16:39:30 -0400

    Update index.js

--- server/index.js
+++ server/index.js
@@ -8,31 +8,32 @@ import cors from "cors";
 import dotenv from "dotenv";
 import { resolvers } from "./graphql/resolvers/resolver.js";
 
-dotenv.config();  // Load environment variables
+dotenv.config();
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const app = express();
 
-// Set up CORS
 app.use(cors({
-  origin: [process.env.CORS_ORIGIN, "https://capstone-frontend-5ide.onrender.com"], // Fetch from environment
+  origin: [process.env.CORS_ORIGIN, "https://capstone-frontend-5ide.onrender.com"],
   credentials: true,
 }));
 
 app.use(express.json());
 
-// Load GraphQL schema file
+app.use(async (req, res, next) => {
+  await new Promise(resolve => setTimeout(resolve, 300000));
+  next();
+});
+
 const typeDefs = fs.readFileSync(
   path.join(__dirname, "graphql/schemas/schema.graphql"),
   "utf-8"
 );
 
-// Connect to MongoDB using Mongoose
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log("MongoDB connected"))
   .catch((err) => console.log("Error connecting to MongoDB", err));
 
-// Define and start Apollo Server
 const startServer = async () => {
   const server = new ApolloServer({
     typeDefs,
@@ -46,16 +47,13 @@ const startServer = async () => {
 
   const port = process.env.PORT || 4005;
 
-  // Start the Express server
   app.listen(port, "0.0.0.0", () => {
     console.log(`🚀 GraphQL Server is running at http://localhost:${port}${server.graphqlPath}`);
   });
 };
 
-// Call the async function to start the server
 startServer();
 
-// Gracefully handle shutdowns for MongoDB and the server
 process.on('SIGINT', async () => {
   console.log("Shutting down server...");
   await mongoose.connection.close();
