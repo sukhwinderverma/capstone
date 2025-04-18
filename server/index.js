0f0204d866b781e6512efdf72fd144710d99f64e
sukhwinderverma <145069602+sukhwinderverma@users.noreply.github.com>
2025-04-18 00:44:32 -0400

    Update index.js

--- server/index.js
+++ server/index.js
@@ -21,7 +21,7 @@ app.use(cors({
 app.use(express.json());
 
 app.use(async (req, res, next) => {
-  await new Promise(resolve => setTimeout(resolve, 60000));
+  await new Promise(resolve => setTimeout(resolve, 500));
   next();
 });
 
